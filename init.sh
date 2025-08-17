04e81cd11ee4c43b891a2d8b19695438d8f001b1
sigma2380 <65985975+sigma2380@users.noreply.github.com>
2025-08-17 09:24:54 -0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,9 +1,15 @@
 % Repo prep
 sudo apt-add-repository -y ppa:yktooo/ppa
+wget https://packages.microsoft.com/config/debian/12/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
 sudo apt-get -y update
 
 % Remove Unneeded
 sudo apt-get -y remove firefox
+
+% Add other software
+sudo apt-get -y install git dconf-editor indicator-sound-switcher
 
 % Upgrade all
 sudo apt-get -y upgrade
@@ -14,15 +20,22 @@ sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo apt --fix-broken install
 rm google-chrome-stable_current_amd64.deb
 
-% Add other software
-sudo apt-get -y install git dconf-editor indicator-sound-switcher
-
 % Chezmoi
 cd /
 sudo sh -c "$(curl -fsLS get.chezmoi.io)"
 sudo ln /bin/chezmoi /bin/cz
 cd ~
 cz init https://github.com/sigma2380/dotfiles.git
+
+% Ghostty
+ARCH="$(dpkg --print-architecture)"
+curl -LO https://download.opensuse.org/repositories/home:/clayrisser:/sid/Debian_Unstable/$ARCH/ghostty_1.1.3-2_$ARCH.deb
+sudo apt install ./ghostty_1.1.3-2_$ARCH.deb
+
+% VS Code
+wget -O vscode-latest.deb https://go.microsoft.com/fwlink/?LinkID=760868
+sudo dpkg -i vscode-latest.deb
+sudo apt install -f
 
 % Add Users
 sudo useradd --create-home zach
