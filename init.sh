3389331601f54d953e61ab13123beb60666d911b
sigma2380 <65985975+sigma2380@users.noreply.github.com>
2025-08-17 10:03:41 -0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
-tput setaf 7; echo "Scott's Settings v1.1"
+tput setaf 7; echo "Scott's Settings v1.1.1"; tput sgr0
 
 % Repo prep
-tput setaf 4; echo "Repo Prep"
+tput setaf 2; echo "Repo Prep"; tput sgr0
 sudo apt-add-repository -y ppa:yktooo/ppa
 wget https://packages.microsoft.com/config/debian/12/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
@@ -9,26 +9,26 @@ rm packages-microsoft-prod.deb
 sudo apt-get -y update
 
 % Remove Unneeded Software
-tput setaf 4; echo "Remove Unneeded Software"
+tput setaf 4; echo "Remove Unneeded Software"; tput sgr0
 sudo apt-get -y remove firefox
 
 % Add other software
-tput setaf 4; echo "Add Software in repos"
+tput setaf 4; echo "Add Software in repos"; tput sgr0
 sudo apt-get -y install git dconf-editor indicator-sound-switcher
 
 % Upgrade all
-tput setaf 4; echo "Upgrade all"
+tput setaf 4; echo "Upgrade all"; tput sgr0
 sudo apt-get -y upgrade
 
 % Add Chrome
-tput setaf 4; echo "Add Chrome"
+tput setaf 4; echo "Add Chrome"; tput sgr0
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo apt --fix-broken install
 rm google-chrome-stable_current_amd64.deb
 
 % Chezmoi
-tput setaf 4; echo "Add Chezmoi"
+tput setaf 4; echo "Add Chezmoi"; tput sgr0
 cd /
 sudo sh -c "$(curl -fsLS get.chezmoi.io)"
 sudo ln /bin/chezmoi /bin/cz
@@ -36,28 +36,28 @@ cd ~
 cz init https://github.com/sigma2380/dotfiles.git
 
 % Ghostty
-tput setaf 4; echo "Add Ghostty"
+tput setaf 4; echo "Add Ghostty"; tput sgr0
 ARCH="$(dpkg --print-architecture)"
 curl -LO https://download.opensuse.org/repositories/home:/clayrisser:/sid/Debian_Unstable/$ARCH/ghostty_1.1.3-2_$ARCH.deb
 sudo apt install ./ghostty_1.1.3-2_$ARCH.deb
 
 % VS Code (download only)
-tput setaf 4; echo "Download VS Code"
+tput setaf 4; echo "Download VS Code"; tput sgr0
 wget -O vscode-latest.deb https://go.microsoft.com/fwlink/?LinkID=760868
 
 % Add Users
-tput setaf 4; echo "Add Users"
+tput setaf 4; echo "Add Users"; tput sgr0
 sudo useradd --create-home zach
 
 % Settings
-tput setaf 4; echo "dconf settings"
+tput setaf 4; echo "dconf settings"; tput sgr0
 dconf write /org/cinnamon/desktop/notifications/display-notifications false
 dconf write /org/cinnamon/desktop/interface/clock-use-24h false
 dconf write /org/gnome/desktop/interface/clock-format "'12h'"
 dconf write /com/linuxmint/updates/hide-systray true
 
 % Wallpaper
-tput setaf 4; echo "Wallpaper"
+tput setaf 4; echo "Wallpaper"; tput sgr0
 sudo mkdir /home/public
 sudo chmod 777 /home/public
 cp ~/.local/share/chezmoi/*.jpg ~/.local/share/chezmoi/*.png /home/public
@@ -68,5 +68,5 @@ gsettings set org.cinnamon.desktop.background picture-uri "file:///home/public/s
 % cat ~/.ssh/id_rsa.pub
 % add key to github
 
-tput setaf 7; echo "Complete"
+tput setaf 2; echo "Complete"; tput sgr0
 
