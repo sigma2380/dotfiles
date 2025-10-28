ab0fcd6d24abe4360e55793181772635816c412c
sigma2380 <65985975+sigma2380@users.noreply.github.com>
2025-10-28 16:29:51 -0400

    Update init-chase.sh

--- init-chase.sh
+++ init-chase.sh
@@ -6,16 +6,16 @@ source <(curl -fsSL https://raw.githubusercontent.com/sigma2380/dotfiles/master/
 
 # Wallpaper
 write_section "Wallpaper"
-gsettings set org.cinnamon.desktop.background picture-uri "file:///home/public/frogfly.png"
+gsettings set org.cinnamon.desktop.background picture-uri "file:///home/public/frogfly.jpg"
 
 # Dconf Settings
 write_section "Dconf Settings"
-dconf write /org/cinnamon/theme/name "'Mint-Y-Dark-Green'"
+dconf write /org/cinnamon/theme/name "'Mint-Y-Dark'"
 dconf write /org/cinnamon/desktop/interface/cursor-theme "'Bibata-Modern-Classic'"
-dconf write /org/cinnamon/desktop/interface/gtk-theme "'Mint-Y-Dark-Green'"
-dconf write /org/cinnamon/desktop/interface/icon-theme "'Mint-Y-Green'"
-dconf write /org/gnome/desktop/interface/gtk-theme "'Mint-Y-Dark-Green'"
-dconf write /org/gnome/desktop/interface/icon-theme "'Mint-Y-Green'"
+dconf write /org/cinnamon/desktop/interface/gtk-theme "'Mint-Y-Dark'"
+dconf write /org/cinnamon/desktop/interface/icon-theme "'Mint-Y'"
+dconf write /org/gnome/desktop/interface/gtk-theme "'Mint-Y-Dark'"
+dconf write /org/gnome/desktop/interface/icon-theme "'Mint-Y'"
 
 # Pin Apps
 write_section "Pin Apps"
