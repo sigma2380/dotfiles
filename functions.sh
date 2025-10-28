5b8bb0767311ec30c10a75b454b805db1ff42ae4
sigma2380 <65985975+sigma2380@users.noreply.github.com>
2025-10-28 14:43:23 -0400

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -14,6 +14,7 @@ function add_user() {
     if id "$1" >/dev/null 2>&1; then
         echo "User '$1' exists."
     else
+        echo "Adding $1 user"
         sudo useradd -s /usr/bin/bash --create-home $1
         echo "$1:$1" | sudo chpasswd
     fi
