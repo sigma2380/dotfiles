0e3b0ef63fce71cf8dcd36dbb959ab399547125e
sigma2380 <65985975+sigma2380@users.noreply.github.com>
2025-10-28 14:38:21 -0400

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -9,3 +9,12 @@ function write_section() {
   echo "$1"
   tput sgr0
 }
+
+function add_user() {
+    if id "$1" >/dev/null 2>&1; then
+        echo "User '$1' exists."
+    else
+        sudo useradd -s /usr/bin/bash --create-home $1
+        echo "$1:$1" | sudo chpasswd
+    fi
+}
