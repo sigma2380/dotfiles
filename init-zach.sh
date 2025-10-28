5fd2997cf658092f11d527b1bc753196eef51dda
sigma2380 <65985975+sigma2380@users.noreply.github.com>
2025-10-28 16:03:48 -0400

    Update init-zach.sh

--- init-zach.sh
+++ init-zach.sh
@@ -5,11 +5,11 @@ write_title "Zach's Settings v1.1"
 source <(curl -fsSL https://raw.githubusercontent.com/sigma2380/dotfiles/master/init-common.sh)
 
 # Wallpaper
-write_heading "Wallpaper"
+write_section "Wallpaper"
 gsettings set org.cinnamon.desktop.background picture-uri "file:///home/public/rockyshore-3.jpg"
 
 # Dconf Settings
-write_heading "Dconf Settings"
+write_section "Dconf Settings"
 dconf write /org/cinnamon/theme/name "'Mint-Y-Dark-Orange'"
 dconf write /org/cinnamon/desktop/interface/cursor-theme "'Bibata-Modern-Classic'"
 dconf write /org/cinnamon/desktop/interface/gtk-theme "'Mint-Y-Dark-Orange'"
@@ -18,7 +18,7 @@ dconf write /org/gnome/desktop/interface/gtk-theme "'Mint-Y-Dark-Orange'"
 dconf write /org/gnome/desktop/interface/icon-theme "'Mint-Y-Orange'"
 
 # Pin Apps
-write_heading "Pin Apps"
+write_section "Pin Apps"
 cp ~/.config/cinnamon/spices/grouped-window-list@cinnamon.org/2.json ~/2.cinpanel.bk
 if ! grep -q "chrome" "~/.config/cinnamon/spices/grouped-window-list@cinnamon.org/2.json"; then
     sed -i '361 i\            "google-chrome.desktop",' ~/.config/cinnamon/spices/grouped-window-list@cinnamon.org/2.json
@@ -27,7 +27,7 @@ else
 fi
 
 # Extension Customizations
-write_heading "Extension Customizations"
+write_section "Extension Customizations"
 cp /home/public/bmw-zach.json ~/.config/cinnamon/spices/CinnamonBurnMyWindows\@klangman/CinnamonBurnMyWindows\@klangman.json
 cp /home/public/tp.json ~/.config/cinnamon/spices/transparent-panels\@germanfr/transparent-panels\@germanfr.json
 
