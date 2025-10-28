cb5fb29b45d53f5d3b60fcc1ab9ec0813c5ac89d
sigma2380 <65985975+sigma2380@users.noreply.github.com>
2025-10-28 16:14:16 -0400

    Create init-katie.sh

---
+++ init-katie.sh
@@ -0,0 +1,37 @@
+source <(curl -fsSL https://raw.githubusercontent.com/sigma2380/dotfiles/master/functions.sh)
+write_title "Katies's Settings v1.1"
+
+# Common settings
+source <(curl -fsSL https://raw.githubusercontent.com/sigma2380/dotfiles/master/init-common.sh)
+
+# Wallpaper
+write_heading "Wallpaper"
+gsettings set org.cinnamon.desktop.background picture-uri "file:///home/public/bunny.jpg"
+
+# Dconf Settings
+write_heading "Dconf Settings"
+dconf write /org/cinnamon/theme/name "'Mint-Y-Dark-Purple'"
+dconf write /org/cinnamon/desktop/interface/cursor-theme "'Bibata-Modern-Classic'"
+dconf write /org/cinnamon/desktop/interface/gtk-theme "'Mint-Y-Dark-Purple'"
+dconf write /org/cinnamon/desktop/interface/icon-theme "'Mint-Y-Purple'"
+dconf write /org/gnome/desktop/interface/gtk-theme "'Mint-Y-Dark-Purple'"
+dconf write /org/gnome/desktop/interface/icon-theme "'Mint-Y-Purple'"
+
+# Pin Apps
+write_heading "Pin Apps"
+cp ~/.config/cinnamon/spices/grouped-window-list@cinnamon.org/2.json ~/2.cinpanel.bk
+if ! grep -q "thunderbird" "~/.config/cinnamon/spices/grouped-window-list@cinnamon.org/2.json"; then
+    sed -i '361 i\            "google-chrome.desktop",' ~/.config/cinnamon/spices/grouped-window-list@cinnamon.org/2.json
+else
+    echo "Shortcuts already pinned."
+fi
+
+# Extension Customizations
+write_heading "Extension Customizations"
+cp /home/public/bmw-scott.json ~/.config/cinnamon/spices/CinnamonBurnMyWindows\@klangman/CinnamonBurnMyWindows\@klangman.json
+cp /home/public/tp.json ~/.config/cinnamon/spices/transparent-panels\@germanfr/transparent-panels\@germanfr.json
+
+cinnamon --replace &
+sleep 2
+
+write_title "Complete"
