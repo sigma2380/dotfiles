dac742a7ef2f90471d8e3f613c2c139fb6438719
sigma2380 <65985975+sigma2380@users.noreply.github.com>
2025-10-28 15:27:33 -0400

    Update init-common.sh

--- init-common.sh
+++ init-common.sh
@@ -1,4 +1,4 @@
-curl -fsSL https://raw.githubusercontent.com/sigma2380/dotfiles/master/functions.sh | source
+source <(curl -fsSL https://raw.githubusercontent.com/sigma2380/dotfiles/master/functions.sh)
 write_title "Common Settings v1.0"
 
 #Dconf Settings
