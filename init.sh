ffb2c26ab93ccf6abed95bd5424c44233fa410e8
sigma2380 <65985975+sigma2380@users.noreply.github.com>
2025-10-28 15:21:05 -0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
-source <(curl -fsSL https://raw.githubusercontent.com/sigma2380/dotfiles/master/functions.sh)
+source < (curl -fsSL https://raw.githubusercontent.com/sigma2380/dotfiles/master/functions.sh)
 
-write_title "Scott's Settings v1.5"
+write_title "Scott's Settings v1.6"
 sleep 1
 
 # Repo prep
@@ -32,7 +32,9 @@ fi
 git -C ~/.local/share/dotfiles pull
 sudo mkdir /home/public
 sudo chmod 777 /home/public
-cp ~/.local/share/dotfiles/*.jpg ~/.local/share/dotfiles/*.png /home/public
+cd ~/.local/share/dotfiles
+cp *.jpg *.png *.sh *.json /home/public
+cd ~
 
 # Add Chrome
 write_section "Add Chrome"
@@ -52,69 +54,4 @@ add_user "chase"
 add_user "katie"
 add_user "so"
 
-exit 0
-
-# Wallpaper
-gsettings set org.cinnamon.desktop.background picture-uri "file:///home/public/spiritedaway.png"
-
-
-# Settings
-tput setaf 4; echo "dconf settings"; tput sgr0
-dconf write /org/cinnamon/desktop/notifications/display-notifications false
-dconf write /org/cinnamon/desktop/interface/clock-use-24h false
-dconf write /org/gnome/desktop/interface/clock-format "'12h'"
-dconf write /com/linuxmint/updates/hide-systray true
-dconf write /org/x/apps/portal/color-scheme "'prefer-dark'"
-dconf write /org/cinnamon/theme/name "'Mint-Y-Dark-Teal'"
-dconf write /org/cinnamon/desktop/interface/cursor-theme "'Bibata-Modern-Classic'"
-dconf write /org/cinnamon/desktop/interface/gtk-theme "'Mint-Y-Dark-Teal'"
-dconf write /org/cinnamon/desktop/interface/icon-theme "'Mint-Y-Teal'"
-dconf write /org/gnome/desktop/interface/gtk-theme "'Mint-Y-Dark-Teal'"
-dconf write /org/gnome/desktop/interface/icon-theme "'Mint-Y-Teal'"
-
-# Wallpaper
-tput setaf 4; echo "Wallpaper"; tput sgr0
-sudo mkdir /home/public
-sudo chmod 777 /home/public
-cp ~/.local/share/chezmoi/*.jpg ~/.local/share/chezmoi/*.png /home/public
-gsettings set org.cinnamon.desktop.background picture-uri "file:///home/public/spiritedaway.png"
-
-# Pin Apps
-tput setaf 4; echo "Pin Apps"; tput sgr0
-cp ~/.config/cinnamon/spices/grouped-window-list@cinnamon.org/2.json ~/2.cinpanel.bk
-if ! grep -q "thunderbird" "~/.config/cinnamon/spices/grouped-window-list@cinnamon.org/2.json"; then
-    sed -i '361 i\            "thunderbird.desktop",' ~/.config/cinnamon/spices/grouped-window-list@cinnamon.org/2.json
-    sed -i '361 i\            "google-chrome.desktop",' ~/.config/cinnamon/spices/grouped-window-list@cinnamon.org/2.json
-else
-    echo "Shortcuts already pinned."
-fi
-
-# Keyboard shortcuts
-gsettings set org.cinnamon.desktop.keybindings custom-list "['custom0', 'custom1']"
-gsettings set org.cinnamon.desktop.keybindings.custom-keybinding:/org/cinnamon/desktop/keybindings/custom-keybindings/custom0/ name 'Switch to Zach'
-gsettings set org.cinnamon.desktop.keybindings.custom-keybinding:/org/cinnamon/desktop/keybindings/custom-keybindings/custom0/ command 'dm-tool switch-to-user zach'
-gsettings set org.cinnamon.desktop.keybindings.custom-keybinding:/org/cinnamon/desktop/keybindings/custom-keybindings/custom0/ binding "['<Primary><Alt><Shift>z']"
-gsettings set org.cinnamon.desktop.keybindings.custom-keybinding:/org/cinnamon/desktop/keybindings/custom-keybindings/custom1/ name 'Switch to Chase'
-gsettings set org.cinnamon.desktop.keybindings.custom-keybinding:/org/cinnamon/desktop/keybindings/custom-keybindings/custom1/ command 'dm-tool switch-to-user chase'
-gsettings set org.cinnamon.desktop.keybindings.custom-keybinding:/org/cinnamon/desktop/keybindings/custom-keybindings/custom1/ binding "['<Primary><Alt><Shift>c']"
-gsettings set org.cinnamon.desktop.keybindings.media-keys area-screenshot-clip "['<Shift>Print', '<Shift><Super>s']"
-
-# Extensions
-tput setaf 4; echo "Desktop Extensions"; tput sgr0
-curl "https://cinnamon-spices.linuxmint.com/files/extensions/transparent-panels@germanfr.zip" > /tmp/tp.zip && unzip -o /tmp/tp.zip -d ~/.local/share/cinnamon/extensions
-rm /tmp/tp.zip
-curl "https://cinnamon-spices.linuxmint.com/files/extensions/CinnamonBurnMyWindows@klangman.zip" > /tmp/bmw.zip && unzip -o /tmp/bmw.zip -d ~/.local/share/cinnamon/extensions
-rm /tmp/bmw.zip
-dconf write /org/cinnamon/enabled-extensions "['transparent-panels@germanfr', 'CinnamonBurnMyWindows@klangman']"
-sleep 1
-cinnamon --replace &
-sleep 2
-cp ~/.local/share/chezmoi/*.json /home/public
-cp /home/public/bmw-scott.json ~/.config/cinnamon/spices/CinnamonBurnMyWindows\@klangman/CinnamonBurnMyWindows\@klangman.json
-cp /home/public/tp.json ~/.config/cinnamon/spices/transparent-panels\@germanfr/transparent-panels\@germanfr.json
-
-# Zach script
-cp ~/.local/share/chezmoi/init-zach.sh /home/public
-
-tput setaf 2; echo "Complete"; tput sgr0
-
+write_title "Complete"
