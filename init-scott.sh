65aec75aa104bd159730e487bd478199fbc7e7bd
sigma2380 <65985975+sigma2380@users.noreply.github.com>
2025-10-28 15:42:33 -0400

    Update init-scott.sh

--- init-scott.sh
+++ init-scott.sh
@@ -1,5 +1,5 @@
 source < (curl -fsSL https://raw.githubusercontent.com/sigma2380/dotfiles/master/functions.sh)
-write_title "Zach's Settings v1.1"
+write_title "Scott's Settings v1.1"
 
 # Common settings
 source < (curl -fsSL https://raw.githubusercontent.com/sigma2380/dotfiles/master/init-common.sh)
